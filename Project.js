c91e419d7e0c2ae0386c48bf390dbdefdcbd50cd
Kyle <ksterne2@msudenver.edu>
2024-02-23 17:47:52 -0700

    .js file with read and write

---
+++ Project.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+// Read Python file
+fs.readFile('test.py', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading Python file:', err);
+        return;
+    }
+
+    // Print Python file contents
+    console.log('Python file contents:');
+    console.log(data);
+
+    // Write Python file contents to a text file
+    fs.writeFile('output.txt', data, 'utf8', (err) => {
+        if (err) {
+            console.error('Error writing to text file:', err);
+            return;
+        }
+        console.log('Python file contents successfully written to output.txt');
+    });
+});
